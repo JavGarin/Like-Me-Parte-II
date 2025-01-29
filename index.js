4392d5b5bd0b22867014929e7dc1f9504b0904e0
Javier Garin <dev.javiergarin@gmail.com>
2025-01-29 20:33:37 -0300

    update files

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(cors());
-app.use(express.json());// para que express entienda los JSON
+app.use(express.json());
 
 // la conexión a la base de datos
 const pool = new Pool({
@@ -17,6 +17,7 @@ const pool = new Pool({
     password: "12251225",
     port: 5432,
 });
+
 // ruta para obtener todos los posts
 app.get("/posts", async (req, res) => {
     try {
@@ -28,29 +29,18 @@ app.get("/posts", async (req, res) => {
     }
 });
 
+// ruta para crear un post
 app.post("/posts", async (req, res) => {
     const { titulo, img, descripcion } = req.body;
     try {
         const query = "INSERT INTO posts (titulo, img, descripcion) VALUES ($1, $2, $3) RETURNING *";
         const values = [titulo, img, descripcion];
         const result = await pool.query(query, values);
-        console.log("Post creado:", result.rows[0]); // imprime el post creado
+        console.log("Post creado:", result.rows[0]);
         res.status(201).json(result.rows[0]);
     } catch (error) {
         console.error("Error al crear el post", error);
         res.status(500).json({ message: "Error al crear el post" });
-    }
-});
-
-// ruta para eliminar un post
-app.delete("/posts/:id", async (req, res) => {
-    const { id } = req.params;
-    try {
-        await pool.query("DELETE FROM posts WHERE id = $1", [id]);
-        res.status(200).json({ message: "Post eliminado correctamente" });
-    } catch (error) {
-        console.error("Error al eliminar el post", error);
-        res.status(500).json({ message: "Error al eliminar el post" });
     }
 });
 
@@ -63,6 +53,18 @@ app.put("/posts/like/:id", async (req, res) => {
     } catch (error) {
         console.error("Error al dar like al post", error);
         res.status(500).json({ message: "Error al dar like al post" });
+    }
+});
+
+// ruta para eliminar un post
+app.delete("/posts/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        await pool.query("DELETE FROM posts WHERE id = $1", [id]);
+        res.status(200).json({ message: "Post eliminado correctamente" });
+    } catch (error) {
+        console.error("Error al eliminar el post", error);
+        res.status(500).json({ message: "Error al eliminar el post" });
     }
 });
 
